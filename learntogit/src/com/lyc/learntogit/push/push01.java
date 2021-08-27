299f3e0b000018711d71c48579262c2bc174379e
liyc21 <liyc21@asiainfo.com>
2021-08-27 11:54:55 +0800

    first commit

--- learntogit/src/com/lyc/learntogit/push/push01.java
+++ learntogit/src/com/lyc/learntogit/push/push01.java
@@ -2,6 +2,7 @@ package com.lyc.learntogit.push;
 
 public class push01 {
     public static void main(String[] args) {
-        System.out.println("push");
+//        System.out.println("push");
+        System.out.println("create new branch");
     }
 }
